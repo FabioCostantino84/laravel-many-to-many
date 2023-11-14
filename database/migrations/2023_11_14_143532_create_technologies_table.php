e5a5e758592f6e3e04c07ce1f4b58ff57d3ec0ff
Fabio Costantino <fabiocostantino84@gmail.com>
2023-11-14 17:25:23 +0100

    add migration

--- database/migrations/2023_11_14_143532_create_technologies_table.php
+++ database/migrations/2023_11_14_143532_create_technologies_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('technologies', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name_tech');
+
+
             $table->timestamps();
         });
     }
